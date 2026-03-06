cd9ee2d2bcf862a39eccfb2e8f8b707179c3153d
ProSachi <syosa@cesde.net>
2026-03-06 17:52:36 -0500

    guias completas

---
+++ Ejercicios/28022026/clase51/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Contactenos from './pages/Contactenos'
+
+function App() {
+
+  return (
+<div>
+<Navbar />
+<Contactenos />
+</div>
+  )
+}
+
+export default App
